10954ba66b2e5484f5d410d957e02e5aafd18ebf
justdilah <nurdilah352@gmail.com>
2023-03-05 20:59:39 +0800

    Made changes

--- Assignment1/Grid.java
+++ Assignment1/Grid.java
@@ -2,21 +2,25 @@ package Assignment1;
 
 public class Grid {
 
-    private int[][] map;
+    private GridState[][] map;
     public Grid(int row, int col){
         CreateGridMap(row,col);
     }
 
     private void CreateGridMap(int rows,int cols){
-        map = new int[rows][cols];
+
+        map = new GridState[rows][cols];
         for (int r=0; r<rows; r++) {
             for (int c=0; c<cols; c++) {
-                map[r][c] = 0;
+                map[r][c] = new GridState(r, c);
             }
         }
+
+        map[0][0].setStateReward(0);
+
     }
 
-    protected int[][] getGridMap(){
+    protected GridState[][] getGridMap(){
         return this.map;
     }
 }
